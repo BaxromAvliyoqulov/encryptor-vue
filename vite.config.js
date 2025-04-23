e885e5a02d7bf9f84eed319694cb0d4ffba87312
BaxromAvliyoqulov <avliyoqulovbaxrom99@gmail.com>
2025-04-23 10:48:25 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+	base: "/",
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		sourcemap: false,
+		minify: "terser",
+	},
+});
